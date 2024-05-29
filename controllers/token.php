c371655dfdd96f6186834f9b676af21d6169a8ea
AlexisVS <alexis.vansan1440@gmail.com>
2024-05-29 17:46:38 +0200

    update

--- controllers/token.php
+++ controllers/token.php
@@ -4,89 +4,65 @@
  * Ask for a backup token.
  *
  * @param array{JWT: string, instance: string} $data
- * @return array{code: int, message: string}
+ * @return array{
+ *   code: int,
+ *   message: array{
+ *     token: string,
+ *     credentials: array{
+ *         username: string,
+ *         password: string
+ * }}}
+ * @throws Exception
  */
 function token(array $data): array
 {
     $status_code = 201;
     $message = '';
 
-
     if (!isset($data['JWT']) || !isset($data['instance'])) {
-        $status_code = 400;
-        $message = 'Bad Request';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Bad Request', 400);
     }
-
 
     // read the backup_tokens.txt file and if lines are more than 10, return status code 429
     $backup_tokens = file('/home/status/backup_tokens.txt', FILE_IGNORE_NEW_LINES);
     if (count($backup_tokens) >= 10) {
-        $status_code = 429;
-        $message = 'Too Many Backup attempts';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Too Many Backup attempts', 429);
     }
-
 
     // Get the backup host JWT
     $backup_host_jwt = file_get_contents('/home/status/jwt.txt');
-
     if ($backup_host_jwt === false) {
-        $status_code = 500;
-        $message = 'Server Error while reading backup host JWT';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Server Error while reading backup host JWT', 500);
     }
 
     // Create a backup token with the backup host JWT and the B2Host JWT + instance name
     $backup_token = openssl_encrypt($data['JWT'] . $data['instance'], 'AES-256-CBC', $backup_host_jwt);
-
     if ($backup_token === false) {
-        $status_code = 500;
-        $message = 'Server Error while creating backup token';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Server Error while creating backup token', 500);
     }
 
     // Append the backup token to the backup_tokens.txt file
     $backup_token_wrote = file_put_contents('/home/status/backup_tokens.txt', $backup_token . PHP_EOL, FILE_APPEND);
-
     if ($backup_token_wrote === false) {
-        $status_code = 500;
-        $message = 'Server Error while writing backup token';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Server Error while writing backup token', 500);
     }
 
     // Escape input to prevent shell injection
-    $escapedUsername = escapeshellarg($data['instance']);
-    $escapedPassword = escapeshellarg($backup_token);
+    $username = escapeshellarg($data['instance']);
+    $password = escapeshellarg($backup_token);
 
-    // Create a new user with a home directory and set the shell to nologin
-    exec("sudo useradd -m -s /usr/sbin/nologin $escapedUsername");
+    // Create a new user and set the shell to nologin
+    exec("sudo useradd -s /usr/sbin/nologin $username");
 
     // Set the user password
-    exec("echo '$escapedUsername:$escapedPassword' | sudo chpasswd");
+    exec("echo '$username:$password' | sudo chpasswd");
 
     $message = [
-        'password' => $backup_token
+        'token' => $backup_token,
+        'credentials' => [
+            'username' => $username,
+            'password' => $password
+        ],
     ];
 
     return [
--- controllers/tokenRelease.php
+++ controllers/tokenRelease.php
@@ -6,6 +6,7 @@
  *
  * @param array{JWT: string, instance: string} $data
  * @return array{code: int, message: string}
+ * @throws Exception
  */
 
 function tokenRelease(array $data): array
@@ -14,54 +15,27 @@ function tokenRelease(array $data): array
     $message = '';
 
     if (!isset($data['JWT']) || !isset($data['instance'])) {
-        $status_code = 400;
-        $message = 'Bad Request';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Bad Request', 400);
     }
 
     // Get the backup host JWT
     $backup_host_jwt = file_get_contents('/home/status/jwt.txt');
-
     if ($backup_host_jwt === false) {
-        $status_code = 500;
-        $message = 'Server Error while reading backup host JWT';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Server Error while reading backup host JWT', 500);
     }
 
     // Create a backup token with the backup host JWT and the B2Host JWT + instance name
     $backup_token = openssl_encrypt($data['JWT'] . $data['instance'], 'AES-256-CBC', $backup_host_jwt);
-
     if ($backup_token === false) {
-        $status_code = 500;
-        $message = 'Server Error while creating backup token';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Server Error while creating backup token', 500);
     }
 
     // Remove the token from the backup_tokens.txt file
     $backup_tokens = file('/home/status/backup_tokens.txt', FILE_IGNORE_NEW_LINES);
     $backup_tokens = array_diff($backup_tokens, [$backup_token]);
     $backup_token_wrote = file_put_contents('/home/status/backup_tokens.txt', implode("\n", $backup_tokens));
-
     if ($backup_token_wrote === false) {
-        $status_code = 500;
-        $message = 'Server Error while writing backup token';
-
-        return [
-            'code' => $status_code,
-            'message' => $message
-        ];
+        throw new Exception('Server Error while writing backup token', 500);
     }
 
     $message = 'Token released';
